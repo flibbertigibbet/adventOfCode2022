89ce49b63dd168c70baa28b8a9d8ddad56c025cb
Kathryn Killebrew <kathryn.killebrew@gmail.com>
2022-12-01 10:24:07 -0500

    First puzzle day 1

---
+++ day1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+with open("data_day1.txt") as data_file:
+    data = [x.strip() for x in data_file]
+    
+total = 0
+elves = []
+
+for x in data:
+    if x == "":
+        elves.append(total)
+        total = 0
+    else:
+        total += int(x)
+
+print(max(elves))
