fa27df0850c637fadaadc8c554dfba65dae5003d
Kathryn Killebrew <kathryn.killebrew@gmail.com>
2022-12-03 13:54:10 -0500

    Day 3 second part

--- day3.py
+++ day3.py
@@ -12,7 +12,9 @@ for idx, c in enumerate(map(chr, range(ord("A"), ord("Z") + 1)), 1):
 
 with open("data_day3.txt") as data_file:
     data = [x.strip() for x in data_file]
+    
 
+##### first puzzle
 total_sack_priorities = 0
 
 for sack in data:
@@ -23,3 +25,25 @@ for sack in data:
     total_sack_priorities += priorities[common]
 
 print(f'total: {total_sack_priorities}')
+
+
+###### second puzzle
+group = []
+elf_idx = 0
+badge_priorites = 0
+
+for sack in data:
+    if elf_idx > 2:
+        common = group[0].intersection(group[1]).intersection(group[2]).pop()
+        badge_priorites += priorities[common]
+        elf_idx = 0
+        group = []
+
+    group.append(set(sack))
+    elf_idx += 1
+
+# last group
+common = group[0].intersection(group[1]).intersection(group[2]).pop()
+badge_priorites += priorities[common]
+
+print(f'total badge priorities: {badge_priorites}')
