7f9117de211837a70ed7e3c0c7b60ab82f1dedfb
Kathryn Killebrew <kathryn.killebrew@gmail.com>
2022-12-04 00:24:33 -0500

    Day 4 part 2

--- day4.py
+++ day4.py
@@ -2,17 +2,23 @@
 
 with open("data_day4.txt") as data_file:
     data = [x.strip() for x in data_file]
-    
-# print(f"got data: {data}")
 
 contained = 0
+partial = 0
 for x in data:
     elf1, elf2 = x.split(',')
-    elf1min, elf1max = elf1.split('-')
-    elf2min, elf2max = elf2.split('-')
-    if int(elf1min) <= int(elf2min) and int(elf1max) >= int(elf2max):
+    elf1min, elf1max = [int(x) for x in elf1.split('-')]
+    elf2min, elf2max = [int(x) for x in elf2.split('-')]
+
+    # part 1
+    if elf1min <= elf2min and elf1max >= elf2max:
         contained += 1
-    elif int(elf2min) <= int(elf1min) and int(elf2max) >= int(elf1max):
+    elif elf2min <= elf1min and elf2max >= elf1max:
         contained += 1
 
+    # part 2
+    if (elf1min <= elf2max and elf1max >= elf2max) or (elf2min <= elf1max and elf2max >= elf1max):
+        partial += 1
+
 print(f"{contained} out of {len(data)} elf pairs fully overlap")
+print(f"{partial} elf pairs partially overlap")
