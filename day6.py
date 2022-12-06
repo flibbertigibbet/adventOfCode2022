93f6ac71f146b7c1b6ae0984d6d7e4e8355e8f1f
Kathryn Killebrew <kathryn.killebrew@gmail.com>
2022-12-06 00:27:16 -0500

    Day 6 part 2

--- day6.py
+++ day6.py
@@ -5,9 +5,11 @@ with open("data_day6.txt") as data_file:
 
 data = data[0]
 
-for x in range(3, len(data)):
-    if (len(set(data[x-4:x])) == 4):
-        print(f"Unique set {data[x-4:x]} found at {x}")
+SIZE = 14
+
+for x in range(SIZE, len(data)):
+    if (len(set(data[x-SIZE:x])) == SIZE):
+        print(f"Unique set {data[x-SIZE:x]} found at {x}")
         break
     
 print(f'done')
