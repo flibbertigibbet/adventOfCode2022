2e4229bfba4e1dd78700ed147a91ee38cab6b204
Kathryn Killebrew <kathryn.killebrew@gmail.com>
2022-12-09 03:28:04 -0600

    Day 9 part 2

--- day9.py
+++ day9.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
+SIZE = 10
 
 with open("data_day9.txt") as data_file:
     data = [x.strip() for x in data_file]
 
-hrow = 0
-hcol = 0
-trow = 0
-tcol = 0
+rows = [0] * SIZE
+cols = [0] * SIZE
 visited = set()
 for x in data:
     dir, dist = x.split(' ')
@@ -14,51 +13,52 @@ for x in data:
     print(f'\n\n{dir} {dist} spaces')
     for i in range(1, dist+1):
         if dir == 'R':
-            hcol += 1
+            cols[0] += 1
         elif dir == 'L':
-            hcol -= 1
+            cols[0] -= 1
         elif dir == 'U':
-            hrow += 1
+            rows[0] += 1
         elif dir == 'D':
-            hrow -= 1
+            rows[0] -= 1
     
-        if hrow == trow:
-            if hcol > (tcol + 1):
-                tcol += 1
-            elif hcol < (tcol - 1):
-                tcol -= 1
-        elif hcol == tcol:
-            if hrow > (trow + 1):
-                trow += 1
-            elif hrow < (trow - 1):
-                trow -= 1
-        else: # move diagonally
-            if hrow > (trow + 1):
-                trow += 1
-                if hcol > tcol:
-                    tcol +=1
-                else:
-                    tcol -= 1
-            elif hrow < (trow - 1):
-                trow -= 1
-                if hcol > tcol:
-                    tcol +=1
-                else:
-                    tcol -= 1
-            if hcol > (tcol + 1):
-                tcol += 1
-                if hrow > trow:
-                    trow += 1
-                else:
-                    trow -= 1
-            elif hcol < (tcol - 1):
-                tcol -= 1
-                if hrow > trow:
-                    trow += 1
-                else:
-                    trow -= 1
+        for t in range(1, SIZE):
+            if rows[t-1] == rows[t]:
+                if cols[t-1] > (cols[t] + 1):
+                    cols[t] += 1
+                elif cols[t-1] < (cols[t] - 1):
+                    cols[t] -= 1
+            elif cols[t-1] == cols[t]:
+                if rows[t-1] > (rows[t] + 1):
+                    rows[t] += 1
+                elif rows[t-1] < (rows[t] - 1):
+                    rows[t] -= 1
+            else: # move diagonally
+                if rows[t-1] > (rows[t] + 1):
+                    rows[t] += 1
+                    if cols[t-1] > cols[t]:
+                        cols[t] +=1
+                    else:
+                        cols[t] -= 1
+                elif rows[t-1] < (rows[t] - 1):
+                    rows[t] -= 1
+                    if cols[t-1] > cols[t]:
+                        cols[t] +=1
+                    else:
+                        cols[t] -= 1
+                if cols[t-1] > (cols[t] + 1):
+                    cols[t] += 1
+                    if rows[t-1] > rows[t]:
+                        rows[t] += 1
+                    else:
+                        rows[t] -= 1
+                elif cols[t-1] < (cols[t] - 1):
+                    cols[t] -= 1
+                    if rows[t-1] > rows[t]:
+                        rows[t] += 1
+                    else:
+                        rows[t] -= 1
     
-        visited.add(f'{trow}:{tcol}')
-        print(f'move head to {hrow}:{hcol} tail to {trow}:{tcol}')
+        visited.add(f'{rows[SIZE-1]}:{cols[SIZE-1]}')
+        print(f'move head to {rows[0]}:{cols[0]} tail to {rows[SIZE-1]}:{cols[SIZE-1]}')
 
 print(f'\ntail visited {len(visited)} unique spots')
