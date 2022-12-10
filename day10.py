aba68a76e61ede14cea30721d42c74458de0d9a6
Kathryn Killebrew <kathryn.killebrew@gmail.com>
2022-12-10 01:08:41 -0500

    Day 10 part 2

--- day10.py
+++ day10.py
@@ -5,22 +5,39 @@ with open("data_day10.txt") as data_file:
 
 cycles = [x for x in range(20, 260, 40)]
 
+# part 2
+WIDTH = 40
+
+def draw(cycle, xreg):
+    pixel = cycle - 1 - ((cycle // WIDTH) * WIDTH)
+    if xreg >= pixel - 1 and xreg <= pixel + 1:
+        print('#', end='')
+    else:
+        print('.', end='')
+    
+    if cycle % WIDTH == 0:
+        print('')
+
 xreg = 1
 cycle = 1
 signal_sum = 0
+draw(cycle, xreg)
 for x in data:
     if x.startswith('addx'):
-        if (cycle + 1) in cycles:
-            print(f'adding off cycle {cycle + 1} amt {(cycle + 1) * xreg}')
-            signal_sum += (cycle + 1) * xreg
-        cycle += 2
+        cycle += 1
+        if cycle in cycles:
+            signal_sum += cycle * xreg
+        
+        draw(cycle, xreg)
+        cycle += 1
         _, amt = x.split(' ')
         xreg += int(amt)
+        draw(cycle, xreg)
     else:
         cycle += 1
+        draw(cycle, xreg)
     
     if cycle in cycles:
-        print(f'adding cycle {cycle} amt {cycle * xreg}')
         signal_sum += cycle * xreg
 
-print(f'signal sum: {signal_sum}')
+print(f'\n\nsignal sum: {signal_sum} last cycle was {cycle}')
