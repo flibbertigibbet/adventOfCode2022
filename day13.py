31a204c5439fcb157e8585ab9293769e6af6096f
Kathryn Killebrew <kathryn.killebrew@gmail.com>
2022-12-13 02:26:16 -0500

    Day 13 part 2

--- day13.py
+++ day13.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import ast
+from functools import cmp_to_key
 
 with open("data_day13.txt") as data_file:
     data = [x.strip() for x in data_file]
@@ -22,11 +23,9 @@ def are_in_order(left, right):
     if type(left) == list and type(right) == list:
         for i in range(len(left)):
             if i > len(right) - 1:
-                print(f"right list {right} is shorter than {left}; NOT in order")
                 return -1
             is_in_order = are_in_order(left[i], right[i])
             if is_in_order == 1 or is_in_order == -1:
-                print(f'recursively found for lists {left} and {right} in order? {is_in_order}')
                 return is_in_order
         
         return len(left) < len(right)
@@ -38,25 +37,19 @@ def are_in_order(left, right):
         
         return is_in_order
     elif type(left) == int and type(right) == int:
-        print(f'compare one int and one not: {left} to {right}')
         if left < right:
-            print(f"ints {left} and {right} are in order")
             return 1
         elif left == right:
-            print(f"ints {left} and {right} are the same")
             return 0
         else:
-            print(f"ints {left} and {right} are NOT in order")
             return -1
 
     elif left.startswith('[') and right.startswith('['):
         next_left = ast.literal_eval(left)
         next_right = ast.literal_eval(right)
-        print(f'parse and compare {next_left} to {next_right}')
         return are_in_order(next_left, next_right)
 
     elif left.isnumeric() or right.isnumeric():
-        print('How did we get here for numeric comp?')
         if left.isnumeric():
             is_in_order = are_in_order([left], right)
         else:
@@ -66,13 +59,10 @@ def are_in_order(left, right):
     
     elif len(left) == 0 or len(right) == 0:
         if len(left) == 0 and len(right) > 0:
-            print('left is empty so in order')
             return 1
         elif len(right) == 0 and len(left) > 0:
-            print('right is empty so NOT  in order')
             return -1
         else:
-            print('both empty?!?!?')
             return 0
     else:
         print('\nHow did we get here?!')
@@ -81,9 +71,33 @@ def are_in_order(left, right):
 
 for idx, pair in enumerate(pairs):
     left, right = pair
-    print(f'\nchecking pair {idx+1}')
     if are_in_order(left, right) == 1:
         in_order.append(idx+1)
 
 print(f"\n\nin-order:\n{in_order}")
 print(f"in-order sum: {sum(in_order)}")
+
+# part 2
+packets = ["[[2]]", "[[6]]"]
+for x in data:
+    if len(x.strip()) > 0:
+        packets.append(x)
+
+packets.sort(key=cmp_to_key(are_in_order), reverse=True)
+
+FIRST_DISTRESS = "[[2]]"
+SECOND_DISTRESS = "[[6]]"
+
+first_packet = -1
+second_packet = -1
+for idx, packet in enumerate(packets):
+    if packet == FIRST_DISTRESS:
+        first_packet = idx + 1
+    elif packet == SECOND_DISTRESS:
+        second_packet = idx + 1
+    
+    if first_packet > 0 and second_packet > 0:
+        break
+
+print(f"Found {first_packet} and {second_packet} indices")
+print(f"Multiple is {first_packet * second_packet}")
